f37944ad7af3d6e67e1c6c6f64f80003914349d3
Antonio Martínez <126609650+antmartinez47@users.noreply.github.com>
2024-09-12 23:58:42 +0200

    Upload scripts and log files for the two following settings: - Reproduction of the results reported in TimeMixer paper with the optimized code - TimeMixer-ETTh1: Hyperparameter tunning of TimeMixer with best validation loss as objective for the ETTh1 dataset (same general configuration for lookback period and horizon length as paper)

---
+++ tfm_scripts/long_term_forecast/ETT_script/TimeMixer_ETTh2_unify.sh
@@ -0,0 +1,27 @@
+python3 train_timemixer.py \
+    --model TimeMixer \
+    --data ETTh2 --root_path ./dataset/ETT-small/ --data_path ETTh2.csv \
+    --features M --save_dir ./checkpoints/baseline/ETTh2_96_96 \
+    --seq_len 96 --label_len 0 --pred_len 96 --down_sampling_layers 3 --down_sampling_window 2 --down_sampling_method avg \
+    --enc_in 7 --dec_in 7 --c_out 7 --d_model 16 --d_ff 32 --learning_rate 0.01 --train_epochs 10 --patience 10 --batch_size 16;
+
+python3 train_timemixer.py \
+    --model TimeMixer \
+    --data ETTh2 --root_path ./dataset/ETT-small/ --data_path ETTh2.csv \
+    --features M --save_dir ./checkpoints/baseline/ETTh2_96_192 \
+    --seq_len 96 --label_len 0 --pred_len 192 --down_sampling_layers 3 --down_sampling_window 2 --down_sampling_method avg \
+    --enc_in 7 --dec_in 7 --c_out 7 --d_model 16 --d_ff 32 --learning_rate 0.01 --train_epochs 10 --patience 10 --batch_size 16;
+
+python3 train_timemixer.py \
+    --model TimeMixer \
+    --data ETTh2 --root_path ./dataset/ETT-small/ --data_path ETTh2.csv \
+    --features M --save_dir ./checkpoints/baseline/ETTh2_96_336 \
+    --seq_len 96 --label_len 0 --pred_len 336 --down_sampling_layers 3 --down_sampling_window 2 --down_sampling_method avg \
+    --enc_in 7 --dec_in 7 --c_out 7 --d_model 16 --d_ff 32 --learning_rate 0.01 --train_epochs 10 --patience 10 --batch_size 16;
+
+python3 train_timemixer.py \
+    --model TimeMixer \
+    --data ETTh2 --root_path ./dataset/ETT-small/ --data_path ETTh2.csv \
+    --features M --save_dir ./checkpoints/baseline/ETTh2_96_720 \
+    --seq_len 96 --label_len 0 --pred_len 720 --down_sampling_layers 3 --down_sampling_window 2 --down_sampling_method avg \
+    --enc_in 7 --dec_in 7 --c_out 7 --d_model 16 --d_ff 32 --learning_rate 0.01 --train_epochs 10 --patience 10 --batch_size 16;
